50d4dca34977c5b09534d4c13063d23ea3cc0810
zext <zextdev@mail.ru>
2018-01-20 01:40:17 +0300

    add new base and edm model and new class

---
+++ zextdevapp1/baseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zextdevapp1
+{
+    public abstract class baseClass
+    {
+        public int id;
+        public string firm;
+        public string model;
+        public decimal price;
+
+        public abstract string zextprint();
+    }
+}
